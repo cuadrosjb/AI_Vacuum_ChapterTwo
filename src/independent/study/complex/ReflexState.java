d5a191e4a9a59d7938e14331a0a3fa03e7ef73b5
Jeffrey Cuadros <cuadrosjb@gmail.com>
2017-02-01 15:32:01 -0500

    Up and down added

--- src/independent/study/complex/ReflexState.java
+++ src/independent/study/complex/ReflexState.java
@@ -1,12 +1,18 @@
 package independent.study.complex;
 
 import java.util.List;
+import java.util.Stack;
 
 public class ReflexState {
 	
 	Blueprint blueprint;
 	int row;
 	int col;
+	
+	int pRow;
+	int pCol;
+	
+	Stack<Object> moves;
 	
 	//use of a Stack might facility things .. 
 	
@@ -15,11 +21,43 @@ public class ReflexState {
 		blueprint =  new Blueprint();
 		row = 29;
 		col = 29;
+		moves = new Stack<Object>();
+		
+		pRow = -1;
+		pCol = -1;
 	}
 	
 	
 	public void move(){
 		//move right if possible if we cannot move right then register as wall and move down 
+		
+		if(pRow == row){
+			//bump encounter in this row either an obstacle or wall;
+			//check previous action
+			if(pRow < row){
+				//we went right
+				
+				//set environment at row col as obstacle;
+				blueprint.getBlueprint().get(row).get(col).setWall(true);
+				
+				
+			}else{
+				//we went left
+				
+			}
+			
+			
+		}else if(pCol == col){
+			if(pCol < col){
+				//we went up
+				
+			}else{
+				//we went down
+				
+			}
+			
+			
+		}
 		
 		//list all possible moves
 		List<Object> lst = getAllPossibleMove();
